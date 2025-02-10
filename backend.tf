3fabd998674c173240e534ea7da1acb56d2683dc
jose.gutierrez <jose.gutierrez@spinbyoxxo.com.mx>
2025-02-10 10:38:00 -0600

    Commented remote s3

--- backend.tf
+++ backend.tf
@@ -1,13 +1,13 @@
-terraform {
-  required_version = ">= 1.0.0"
+# terraform {
+#   required_version = ">= 1.0.0"
 
-  backend "s3" {
-    region  = "us-west-1"
-    bucket  = "eg-luis-test-terraform-state"
-    key     = "terraform.tfstate"
-    profile = "me"
-    encrypt = "true"
+#   backend "s3" {
+#     region  = "us-west-1"
+#     bucket  = "eg-luis-test-terraform-state"
+#     key     = "terraform.tfstate"
+#     profile = "me"
+#     encrypt = "true"
 
-    dynamodb_table = "eg-luis-test-terraform-state-lock"
-  }
-}
+#     dynamodb_table = "eg-luis-test-terraform-state-lock"
+#   }
+# }
